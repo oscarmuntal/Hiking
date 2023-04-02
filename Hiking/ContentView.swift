bed9242e4b049faaf37cfbd9956f9bc7b58b5232
Òscar Muntal <oscar.muntal@gmail.com>
2023-04-02 12:29:34 +0200

    Initial Commit

---
+++ Hiking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hiking
+//
+//  Created by Òscar Muntal on 2/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by Òscar Muntal on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
